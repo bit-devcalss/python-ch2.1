a61f2905038e2c688ee1389ad20df56b96b6ce4b
bit-user <iremys@gmail.com>
2018-05-28 09:48:46 +0900

    Initial commit

---
+++ arrignment.py
@@ -0,0 +1,40 @@
+# 대입문
+a=1
+b=a+1
+print(a, b)
+
+# 오류
+# 1+3=c
+
+# 세미콜론으로 구분
+e=3; f=5+2
+print(e, f)
+
+# 여러변수를 한번에 대입
+e, f, g=10, 20, 30
+print(e, f, g)
+
+# 여러개를 같은 값으로 대입
+x=y=z=10
+print(x, y, z)
+
+# 값 교환하기
+a=3
+b=7
+a, b = b, a
+print(a, b)
+
+# 오류
+# a=(b=c+d)
+
+a=1
+print(type(a))
+
+a='hello'
+print(type(a))
+
+
+
+
+
+
---
+++ variable.py
@@ -0,0 +1,40 @@
+import keyword
+
+friend = 1
+a= 10
+my_name = "황일영"
+myName = "유재석"
+_yourName = "이정재"
+member1 = '박명수'
+
+print(friend)
+print(a)
+print(my_name)
+print(_yourName)
+print(member1)
+
+# 에러
+# $friend = 2
+# a! = 1
+# 1abc = 3
+# def = 3
+
+print(keyword.kwlist)
+print(len(keyword.kwlist))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
